18230f3615141486a494b6ef901c183b855065ca
danielfajardomaqueira <d.fajardomaqueira@gmail.com>
2023-01-16 09:07:54 +0100

    PlayerController_Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    private Rigidbody _rigidbody;
+    public float jumpForce = 10f;
+    private bool isOnTheGround = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnTheGround)
+        {
+            isOnTheGround = false;
+            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision otherCollider)
+    {
+        isOnTheGround = true;
     }
 }
